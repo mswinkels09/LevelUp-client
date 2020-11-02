78b5978878cdcdfc4119bbaff41d3b61f5559445
mswinkels09 <mswinkels09@gmail.com>
2020-11-02 10:04:28 -0600

    user can now see list of games along with the details of each game

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,13 +1,14 @@
 import React from "react"
 import { Route } from "react-router-dom"
+import {GameProvider} from "./game/GameProvider"
+import {GameList} from "./game/GameList"
 
 export const ApplicationViews = () => {
     return <>
-        <main style={{
-            margin: "5rem 2rem",
-            backgroundColor: "lightgoldenrodyellow"
-        }}>
-            Application views
-        </main>
+        <GameProvider>
+            <Route exact path="/">
+                <GameList />
+            </Route>
+        </GameProvider>
     </>
 }
